ae74eaf1478e5acd1e75e912e58f99f24e69301c
willrussell <wrussell35@gmail.com>
2019-10-04 23:02:18 -0700

    sherdog parser initial commit

---
+++ scrape.js
@@ -0,0 +1,26 @@
+const parseEventsList = require('./eventParser');
+const request = require("request");
+const cheerio = require("cheerio");
+const moment = require("moment");
+
+request(
+  "https://www.sherdog.com/organizations/Ultimate-Fighting-Championship-UFC-2",
+  (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+
+      const tableRows = $(
+        "#events_list #upcoming_tab .event tbody .odd, #events_list #upcoming_tab .event tbody .even"
+      );
+
+      const collection = parseEventsList($, tableRows);
+      console.log(collection);
+
+      
+      // collection.forEach((item, i) => {
+      // 	console.log(item.title);
+      // 	console.log(item.location);
+      // });
+    }
+  }
+);
