ed3fa24af1cb6758cd5b6a5f4e1f799ac9c4c285
willrussell <wrussell35@gmail.com>
2023-12-21 19:53:57 -0500

    Refactor event scraping and data storage

--- scrape-events.js
+++ scrape-events.js
@@ -1,54 +1,42 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const parseEventsList = require("./eventsParser");
-const moment = require("moment");
-const AWS = require("aws-sdk");
-const fetch = require("node-fetch");
+const moment = require('moment');
+const S3 = require('aws-sdk/clients/s3');
+const fetch = require('node-fetch');
 const { performance } = require('perf_hooks');
-const { some } = require("lodash");
+const { some } = require('lodash');
+const parseEventsList = require('./eventsParser');
 
-const eventUris = [
-  "UFC_EVENTS_URI",
-  "BELLATOR_EVENTS_URI",
-  "ONEFC_EVENTS_URI",
-  "PFL_EVENTS_URI",
-  "FURY_FC_EVENTS_URI",
-  "CONTENDER_SERIES_EVENTS_URI",
-  "FULL_CONTACT_FIGHTING_EVENTS_URI",
-  "UFC_APEX_EVENTS_URI",
-  "URIAH_FABER_A1_COMBAT_EVENTS_URI",
-];
+const eventUris = process.env.URI_COLLECTION.split(',');
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env["ACCESS_KEY_ID"],
-  secretAccessKey: process.env["SECRET_ACCESS_KEY"],
+const s3 = new S3({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
 });
 
 const t0 = performance.now();
 
-const requests = async () => {
-  return await Promise.all(
-    eventUris.map(uri => fetch(process.env[uri]))
-  );
-};
+const requests = async () => Promise.all(
+  eventUris.map((uri) => fetch(process.env[uri])),
+);
 
 async function fetchData() {
   const responses = await requests();
-  const rawData = await Promise.all(responses.map(response => response.text()));
-  const mergedCollection = rawData.flatMap(rawDataItem => parseEventsList(rawDataItem));
+  const rawData = await Promise.all(responses.map((response) => response.text()));
+  const mergedCollection = rawData.flatMap((rawDataItem) => parseEventsList(rawDataItem));
 
   const sortedCollection = mergedCollection.sort(
-    (a, b) => a.unixDate - b.unixDate
+    (a, b) => a.unixDate - b.unixDate,
   );
 
   const hasError = some(responses, ['ok', false]);
 
   if (hasError) {
     throw new Error(
-      `HTTP error! status: ${response[0].status}, ${response[1].status}`
+      `HTTP error! status: ${responses[0].status}, ${responses[1].status}`,
     );
   } else {
-    return await sortedCollection;
+    return sortedCollection;
   }
 }
 
@@ -59,11 +47,11 @@ fetchData()
     const json = JSON.stringify(fileObj);
 
     const params = {
-      ACL: "public-read",
+      ACL: 'public-read',
       Body: json,
-      ContentType: "application/json",
-      Bucket: process.env["S3_BUCKET"],
-      Key: "events-collection.json",
+      ContentType: 'application/json',
+      Bucket: process.env.S3_BUCKET,
+      Key: 'events-collection.json',
     };
 
     s3.putObject(params, (err, results) => {
@@ -71,15 +59,15 @@ fetchData()
       else {
         const t1 = performance.now();
         const duration = moment.duration(t1 - t0).humanize();
-        
-        /* Log some details on success*/
-        collection.forEach((o) => { console.log(`${o.id} : ${o.title}, ${o.subtitle}`) });
-        console.log("-------------------------------------------------");
+
+        /* Log some details on success */
+        collection.forEach((o) => { console.log(`${o.id} : ${o.title}, ${o.subtitle}`); });
+        console.log('-------------------------------------------------');
         console.log(`Events collected: ${collection.length}`);
         console.log(`Task duration: ${(duration)}`);
-        console.log("-------------------------------------------------");
+        console.log('-------------------------------------------------');
         console.log(results); // s3 tag
-        console.log("-------------------------------------------------");
+        console.log('-------------------------------------------------');
       }
     });
   })
